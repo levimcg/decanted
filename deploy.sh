14b21d1f78a2757d5267d725e4d97a8c71b8e293
lmcgrana <lmcgrana@iu.edu>
2017-12-30 16:29:43 -0500

    Adds push step to deploy script

--- deploy.sh
+++ deploy.sh
@@ -23,3 +23,6 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Deploying to Github pages"
+git push orign gh-pages
